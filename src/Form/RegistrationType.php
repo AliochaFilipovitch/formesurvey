600cba25558f31df12aa3e8f59ac405daaf065e9
AliochaFilipovitch <alexis.queune1@gmail.com>
2020-07-21 12:05:36 +0200

    add departement

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -3,10 +3,13 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Entity\Department;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 
 class RegistrationType extends AbstractType
 {
@@ -15,6 +18,10 @@ class RegistrationType extends AbstractType
         $builder
             ->add('email')
             ->add('username')
+            ->add('department', EntityType::class, [
+                'class' => Department::class,
+                'choice_label' => 'name'
+            ])
             ->add('password', PasswordType::class)
             ->add('confirm_password', PasswordType::class)
         ;
