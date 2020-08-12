76d5f3091153c8ec8f0acca0abab86b6318e3504
AliochaFilipovitch <alexis.queune1@gmail.com>
2020-08-12 17:58:16 +0200

    improve backend

--- config/secrets/env/env.list.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array (
-  'KEY_API_GIPHY' => NULL,
-  'KEY_JS_FONTAWESOME' => NULL,
-);
--- config/secrets/prod/prod.list.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array (
-  'KEY_API_PIXABAY' => NULL,
-  'KEY_JS_FONTAWESOME' => NULL,
-);
